65d14fc78578e11ea686aa2c4632f662e9f9e438
Dante Pasionek <dante.pasionek1@gmail.com>
2018-07-25 12:49:21 -0600

    Added Fetch to the service worker

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -34,6 +34,20 @@ self.addEventListener('activate', event => {
     );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
 self.addEventListener('push', function (event) {
     event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
         body: 'Push Notification Subscription Management'
